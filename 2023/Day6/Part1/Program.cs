afc03b39fd395a599959a53de4981aea0994d831
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-05 23:38:20 -0600

    Day 6 CSharp

---
+++ 2023/Day6/Part1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Day 6 Part 1
+
+// Get ways to win for a given time/distance
+int getWaysToWin(int time, int distance) {
+    int ways = 0;
+    for(int i = 0; i<time; i++) {
+        int speed = i;
+        int timeRemain = time - i;
+        int distanceCovered = speed * timeRemain;
+        if(distanceCovered>distance) ways++;
+    }
+    return ways;
+}
+
+// Globals
+int sum = 0;
+
+// Inputs - times and distance records
+List<int> times = new List<int>(){53, 71, 78, 80};
+List<int> distance = new List<int>(){275, 1181, 1215, 1524};
+List<int> ways = new List<int>(){0, 0, 0, 0};
+
+// Iterate time/distance
+for(int i=0; i<times.Count; i++) {
+    ways[i] = getWaysToWin(times[i],distance[i]);
+}
+
+sum = ways[0];
+for(int i=1; i<times.Count; i++) {
+    sum = sum * ways[i];
+}
+
+// Dumpit to crumpit
+Console.WriteLine("DAY 6 PART 1");
+Console.WriteLine($"Sum: {sum}");
---
+++ 2023/Day6/Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Day 6 Part 2
+
+// Get ways to win for a given time/distance
+long getWaysToWin(long time, long distance) {
+    long ways = 0;
+    for(long i = 0; i<time; i++) {
+        long speed = i;
+        long timeRemain = time - i;
+        long distanceCovered = speed * timeRemain;
+        if(distanceCovered>distance) ways++;
+        if(i%100000 == 0) Console.WriteLine("100k Processed");
+    }
+    return ways;
+}
+
+// Globals
+long sum = 0;
+
+// Inputs - times and distance records
+List<long> times = new List<long>(){53717880};
+List<long> distance = new List<long>(){275118112151524};
+List<long> ways = new List<long>(){0};
+
+// Iterate time/distance
+for(int i=0; i<times.Count; i++) {
+    ways[i] = getWaysToWin(times[i],distance[i]);
+}
+
+sum = ways[0];
+for(int i=1; i<times.Count; i++) {
+    sum = sum * ways[i];
+}
+
+// Dumpit to crumpit
+Console.WriteLine("DAY 6 PART 2");
+Console.WriteLine($"Sum: {sum}");
