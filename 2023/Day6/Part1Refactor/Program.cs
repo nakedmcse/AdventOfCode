6d69eefe1dcddaf8efda0539921768c50879b040
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-06 00:36:37 -0600

    Day 6 Refactors

---
+++ 2023/Day6/Part1Refactor/Program.cs
@@ -0,0 +1,37 @@
+﻿// Day 6 Part 1 Refactor
+
+// Get ways to win for a given time/distance
+int getWaysToWin(int time, int distance) {
+    int ways = 0;
+    for(int i = 0; i<time; i++) {
+        int speed = i;
+        int timeRemain = time - i;
+        int distanceCovered = speed * timeRemain;
+        if(distanceCovered>distance) {
+            ways++;
+        }
+    }
+    return ways;
+}
+
+// Globals
+int sum = 0;
+
+// Inputs - times and distance records
+int[] times = new int[] {53, 71, 78, 80};
+int[] distance = new int[] {275, 1181, 1215, 1524};
+int[] ways = new int[] {0, 0, 0, 0};
+
+// Iterate time/distance
+for(int i=0; i<times.Count(); i++) {
+    ways[i] = getWaysToWin(times[i],distance[i]);
+}
+
+sum = ways[0];
+for(int i=1; i<times.Count(); i++) {
+    sum = sum * ways[i];
+}
+
+// Dumpit to crumpit
+Console.WriteLine("DAY 6 PART 1 REFACTOR");
+Console.WriteLine($"Sum: {sum}");
---
+++ 2023/Day6/Part2Refactor/Program.cs
@@ -0,0 +1,26 @@
+﻿// Day 6 Part 2 Refactor
+
+// Get ways to win for a given time/distance
+long getWaysToWin(long time, long distance) {
+    long ways = 0;
+    for(long i = 0; i<time; i++) {
+        long speed = i;
+        long timeRemain = time - i;
+        long distanceCovered = speed * timeRemain;
+        if(distanceCovered>distance) {
+            ways++;
+        }
+    }
+    return ways;
+}
+
+// Inputs - times and distance records
+long times = 53717880;
+long distance = 275118112151524;
+long ways = 0;
+
+ways = getWaysToWin(times,distance);
+
+// Dumpit to crumpit
+Console.WriteLine("DAY 6 PART 2 REFACTOR");
+Console.WriteLine($"Sum: {ways}");
