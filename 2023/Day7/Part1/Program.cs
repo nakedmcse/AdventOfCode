40a1a8ded7c91415ea1abbd40a2e6f8140ba89cb
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-06 21:04:16 -0600

    Boilerplate for Day 7

---
+++ 2023/Day7/Part1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Day 7 Part 1
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+// Globals
+Regex patternDigit = new Regex(@"\d+");
+long sum = 0;
+
+// Functions/Types
+long extractDigit(string target) {
+    var matches = patternDigit.Matches(target);
+    if(matches.Count>0) {
+        if(long.TryParse(matches[0].Value,out long retval)) return retval;
+    }
+    return 0;
+}
+
+// Read Input File
+var lines = new List<string>(File.ReadAllLines("inputfile.txt"));
+
+// Iterate Lines
+foreach (string line in lines) {
+    //Processing here
+}
+
+// Dumpit to crumpit
+Console.WriteLine("Part 1");
+Console.WriteLine($"Sum: {sum}");
