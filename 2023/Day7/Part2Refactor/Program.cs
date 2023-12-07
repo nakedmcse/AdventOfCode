868d629ef749763d00fe8b25a11096b177da31c7
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-07 08:53:03 -0600

    Day 7 Refactors

--- 2023/Day7/Part2Refactor/Program.cs
+++ 2023/Day7/Part2Refactor/Program.cs
@@ -1,2 +1,136 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Part 2 Refactor");
+﻿// Day 7 Part 2
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+// Globals
+Regex patternDigit = new Regex(@"\d+");
+long sum = 0;
+
+// Functions/Types
+long extractDigit(string target) {
+    var matches = patternDigit.Matches(target);
+    if(matches.Count>0) {
+        if(long.TryParse(matches[0].Value,out long retval)) return retval;
+    }
+    return 0;
+}
+
+// Read Input File
+var lines = new List<string>(File.ReadAllLines("inputfile.txt"));
+var hands = new List<hand>();
+
+// Iterate Lines
+foreach (string line in lines) {
+    // Create hand list
+    var splitline = line.Trim().Split(" ");
+    hands.Add(new hand(splitline[0],extractDigit(splitline[1])));
+}
+Console.WriteLine($"Loaded {hands.Count} hands");
+
+// Value hands
+foreach (var h in hands) {
+    h.getHandValue(); // must come before mutate!
+    h.mutateJoker();
+    h.getHandCounts();
+    h.getRank();
+}
+
+// Sort hands
+var sortedHands = hands.OrderBy(x => x.rank).
+    ThenBy(x => x.cardValue[0]).
+    ThenBy(x => x.cardValue[1]).
+    ThenBy(x => x.cardValue[2]).
+    ThenBy(x => x.cardValue[3]).
+    ThenBy(x => x.cardValue[4]).
+    ToList();
+
+// Figure the sum
+for(int i = 0; i<sortedHands.Count; i++) {
+    sum += (sortedHands[i].bet * (long)(i+1));
+}
+
+// Dumpit to crumpit
+Console.WriteLine("Part 2");
+Console.WriteLine($"Sum: {sum}");
+
+// Class
+class hand {
+    public string cards { get; set; }
+    public long bet { get; set; }
+    public long rank { get; set; }
+    public int[] cardValue { get; set; }
+    public Dictionary<char,int> counts { get; set; }
+
+    public hand(string cds, long b) {
+        cards = cds;
+        bet = b;
+        rank = 0;
+        cardValue = new int[5];
+        counts = new Dictionary<char, int>();
+    }
+
+    public void getHandCounts() {
+        foreach(char c in cards) {
+            if(counts.ContainsKey(c)) {
+                counts[c]++;
+            }
+            else {
+                counts[c]=1;
+            }
+        }
+    }
+
+    public void getRank() {
+        if(counts.Count == 1) {
+            rank = 7;
+        }
+        else if(counts.Count == 2 && counts.Values.Contains(1)) {
+            rank = 6;
+        }
+        else if(counts.Count == 2 && counts.Values.Contains(3) && counts.Values.Contains(2)) {
+            rank = 5;
+        }
+        else if(counts.Count == 3 && counts.Values.Contains(3) && counts.Values.Contains(1)) {
+            rank = 4;
+        }
+        else if(counts.Count == 3 && counts.Values.Contains(2) && counts.Values.Contains(1)) {
+            rank = 3;
+        }
+        else if(counts.Count == 4) {
+            rank = 2;
+        }
+        else {
+            rank = 1;
+        }
+    }
+
+    public void mutateJoker() {
+        if(!cards.Contains("J")) {
+            return;
+        }
+        string cardValues = "23456789TQKA";
+        long maxRank = 0;
+        string mutatedCards = cards;
+        foreach (var c in cardValues) {
+            var tempHand = new hand(cards,bet);
+            tempHand.cards = tempHand.cards.Replace('J',c);
+            tempHand.getHandCounts();
+            tempHand.getRank();
+            if(tempHand.rank > maxRank) {
+                maxRank = tempHand.rank;
+                mutatedCards = tempHand.cards;
+            }
+        }
+        cards = mutatedCards;
+    }
+
+    public void getHandValue() {
+        string cardValues = "J23456789TQKA";
+        int i = 0;
+        foreach (var c in cards) {
+            cardValue[i] = cardValues.IndexOf(c);
+            i++;
+        }
+    }
+}
