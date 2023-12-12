dbd92b6ed70e79f268a9691bbbc08656d2058693
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-12 11:34:31 -0600

    Part 2 Calc Sample Correct

--- 2023/Day12/part2.ts
+++ 2023/Day12/part2.ts
@@ -2,7 +2,7 @@
 
 // Read input file
 import * as fs from 'fs';
-const fileData: string = fs.readFileSync('inputfile.txt','utf8');
+const fileData: string = fs.readFileSync('sample.txt','utf8');
 const lines: string[] = fileData.split('\n');
 
 // Get number
@@ -89,6 +89,22 @@ function expandCombos(count:number):number {
     return count * (2*count) * (4*count) * (8*count)
 }
 
+// Create prefix
+function createPrefix(pattern:string,firstGroupLen:number,lastGroupLen:number):string {
+    if(extractEndHashes(pattern).length == lastGroupLen) {
+        return '';
+    }
+    if(pattern.endsWith('.')) {
+        return '?';
+    }
+    return (pattern + "?").slice(-firstGroupLen);
+}
+
+// Create postfix
+function createPostfix(pattern:string,lastGroupLen:number):string {
+    return ('?' + pattern.slice(0,lastGroupLen-1));
+}
+
 // Globals
 let sum:number = 0;
 
@@ -103,6 +119,7 @@ for(let line of lines) {
     // Iterate combinations
     let combos:number = 0;
     let prefixCombos:number = 0;
+    let postfixCombos: number = 0;
     const bits:number = 2**places;
     // Original pattern
     for(let i=0; i<bits; i++) {
@@ -113,12 +130,15 @@ for(let line of lines) {
         }
     }
     // Prefix pattern
-    const tailPattern = (pattern + "?").slice(-combinations[0]);
-    let prevEnd:string = extractEndHashes(pattern).length == combinations[combinations.length-1] ? "" : tailPattern;
-
-    const prefixPattern: string = prevEnd + pattern + "?";
+    const postfix:string = createPostfix(pattern,combinations[combinations.length-1]);
+    const prefix:string = createPrefix(pattern,combinations[0],combinations[combinations.length-1]);
+    const prefixPattern: string = prefix + pattern + '?';
+    const postfixPattern: string = prefix + pattern;
     const prefixPlaces:number = prefixPattern.split('?').length - 1;
+    const postfixPlaces:number = postfixPattern.split('?').length - 1;
     const prefixBits:number = 2**prefixPlaces;
+    const postfixBits:number = 2**postfixPlaces;
+    // Calc prefix combo
     for(let i=0; i<prefixBits; i++) {
         let candidate: string = replacePattern(prefixPattern,i,prefixPlaces);
         let candidateGroups: number[] = countGroups(candidate,'#');
@@ -126,8 +146,17 @@ for(let line of lines) {
             prefixCombos++;
         }
     }
+    // Calc postfix combo
+    for(let i=0; i<postfixBits; i++) {
+        let candidate: string = replacePattern(postfixPattern,i,postfixPlaces);
+        let candidateGroups: number[] = countGroups(candidate,'#');
+        if(checkGroups(candidateGroups,combinations)) {
+            postfixCombos++;
+        }
+    }
+
     const totalCombos = combos * prefixCombos**4;
-    console.log(pattern,'--',prevEnd,'--',prefixPattern,'--',combinations,'--',combos,'--',prefixCombos,'--',totalCombos);
+    console.log(pattern,'--',prefixPattern,'--',combinations,'--',combos,'--',prefixCombos,'--',totalCombos);
     sum += totalCombos;
 }
 
