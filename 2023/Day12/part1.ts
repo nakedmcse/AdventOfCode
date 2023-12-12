43940ec89149de4d6d970e9750907eaca387c6ec
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-12 01:42:28 -0600

    Day 12 Part 1 TS

---
+++ 2023/Day12/part1.ts
@@ -0,0 +1,102 @@
+// Day 12 Part 1
+
+// Read input file
+import * as fs from 'fs';
+const fileData: string = fs.readFileSync('inputfile.txt','utf8');
+const lines: string[] = fileData.split('\n');
+
+// Get number
+function getNumber(partstr:string): number {
+    const matches = partstr.match(/\-?\d+/g);
+    return matches && matches[0] ? parseInt(matches[0]) : 0;
+}
+
+// Get groups of hash
+function countGroups(input:string, target:string): number[] {
+    const counts: number[] = [];
+    let curCount = 0;
+
+    for(let i=0; i<input.length; i++) {
+        if(input[i]==target) {
+            curCount++;
+        } else {
+            if(curCount>0) {
+                counts.push(curCount);
+                curCount = 0;
+            }
+        }
+    }
+
+    // Need to add last run
+    if(curCount>0) {
+        counts.push(curCount);
+    }
+
+    return counts;
+}
+
+// Replace ? with . or # depending on binary number
+function replacePattern(input: string, num: number, count:number): string {
+    const binNum = num.toString(2).padStart(count,'0');
+    let binaryIdx = 0;
+    let result = '';
+
+    for(let i=0; i<input.length; i++) {
+        if(input[i]=='?') {
+            if(binaryIdx<binNum.length) {
+                result += binNum[binaryIdx] == '1' ? '#' : '.';
+                binaryIdx++;
+            } else {
+                // Only get here is bits do not match pattern
+                result += '.';
+            }
+        } else {
+            // Other char
+            result += input[i];
+        }
+    }
+
+    return result;
+}
+
+// Check Same Pattern
+function checkGroups(candidate: number[], control: number[]): boolean {
+    if(candidate.length != control.length) {
+        return false;
+    }
+    for(let i = 0; i<candidate.length; i++) {
+        if(candidate[i]!=control[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Globals
+let sum:number = 0;
+
+// Iterate lines
+for(let line of lines) {
+    const splitline = line.split(' ');
+    const pattern = splitline[0];
+    const stringcombo = splitline[1];
+    const combinations: number[] = stringcombo.split(',').map(num => parseInt(num.trim()));
+    const places: number = pattern.split('?').length - 1;
+
+    // Iterate combinations
+    let combos:number = 0;
+    const bits:number = 2**places;
+    for(let i=0; i<bits; i++) {
+        let candidate: string = replacePattern(pattern,i,places);
+        let candidateGroups: number[] = countGroups(candidate,'#');
+        if(checkGroups(candidateGroups,combinations)) {
+            combos++;
+        }
+    }
+    console.log(pattern,'--',combinations,'--',combos);
+    sum += combos;
+}
+
+// Dumpit to Crumpit
+console.log("PART 1");
+console.log("Sum:",sum);
---
+++ 2023/Day12/part2d11.ts
@@ -0,0 +1,122 @@
+// Day 11 Part 2
+
+// Read input file
+import * as fs from 'fs';
+import { get } from 'http';
+import { start } from 'repl';
+const fileData: string = fs.readFileSync('inputfile.txt','utf8');
+const lines: string[] = fileData.split('\n');
+
+// Space class
+class Space {
+    distances: number[];
+    public constructor(public x:number,public y:number,public galaxy:number) {
+        this.distances = [];
+    }
+}
+
+// Get number
+function getNumber(partstr:string): number {
+    const matches = partstr.match(/\-?\d+/g);
+    return matches && matches[0] ? parseInt(matches[0]) : 0;
+}
+
+// Get distance between galaxies
+function getDistance(galaxy1:Space, galaxy2:Space): number {
+    let pixels:number = 0;
+    let dx:number = Math.abs(galaxy1.x - galaxy2.x);
+    let dy:number = Math.abs(galaxy1.y - galaxy2.y);
+    let linelen = Math.floor(Math.sqrt(dx**2 + dy**2));
+
+    let mx:number = (galaxy1.x - galaxy2.x)/linelen;
+    let my:number = (galaxy1.y - galaxy2.y)/linelen;
+   
+    pixels += Math.abs(mx*linelen) + Math.abs(my*linelen);
+    return Math.round(pixels);
+}
+
+// Rotate lines 90 degrees
+function rotate(lines:string[]): string[] {
+    let table: string[][] = lines.map(line => line.split(''));
+    let transpose: string[][] = table[0].map((_, colIndex) => table.map(row => row[colIndex]));
+    let rotated: string[][] = transpose.map(row => row.reverse());
+    let rotatedLines: string[] = rotated.map(row => row.join(''));
+    return rotatedLines;
+}
+
+// Get expansion rows/cols
+function getExpansion(space:string[], isCols:boolean): number[] {
+    let expansions:number[] = [];
+    if(isCols) {
+        // Rotate grid 90 degrees clockwise
+        space = rotate(space);
+    }
+
+    for(let i=0; i<space.length; i++) {
+        if(space[i].split('').every(c => c == '.')) {
+            expansions.push(i);
+        }
+    }
+
+    return expansions;
+}
+
+// Iterate lines
+let x:number = 0, y:number = 0, galaxy:number = 0, sum:number = 0;
+let ux:number = 0, uy:number = 0;
+const starMap:Array<Space> = [];
+
+// Get expansions
+const rowExpansion:number[] = getExpansion(lines,false);
+const colExpansion:number[] = getExpansion(lines, true);
+
+// Extract coords
+for(let line of lines) {
+    x = 0;
+    ux = 0;
+    for(let c of line) { 
+        if(c=='#') {
+            galaxy++;
+            starMap.push(new Space(x,y,galaxy));
+        }
+        if(colExpansion.includes(ux)) {
+            x += 1000000;
+        } else {
+            x++;
+        }
+        ux++;  // untranslated x - so column expansion compared to original x pos
+    }
+    if(rowExpansion.includes(uy)) {
+        y += 1000000;
+    } else {
+        y++;
+    }
+    uy++; // untranslated y - so row expansion compared to original y pos
+}
+
+// Get distances
+for(let i = 0; i<starMap.length; i++) {
+    starMap[i].distances = new Array(starMap.length);
+    for(let j = 0; j<starMap.length; j++) {
+        if(i==j) {
+            starMap[i].distances[j] = 99999;
+            continue;
+        }
+        if(i!=j) {
+            starMap[i].distances[j] = getDistance(starMap[i],starMap[j]);
+        }
+    }
+}
+
+// Sum Distances
+sum = 0;
+for(let galaxy of starMap) {
+    for(let dist of galaxy.distances) {
+        if(dist!=99999) sum += dist;
+    }
+}
+sum = sum/2;
+
+// Dumpit to Crumpit
+console.log("PART 2");
+console.log("Sum:",sum);
