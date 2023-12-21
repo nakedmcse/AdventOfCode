3b61e47c6332b3442032a7d0a80ddef2ad3f15c6
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-21 13:47:59 -0600

    Part 2 solution

--- 2023/Day21/part2.py
+++ 2023/Day21/part2.py
@@ -1,5 +1,4 @@
 # Day 21 Part 2
-import math
 
 # Init Vars
 sum = 0
@@ -7,15 +6,36 @@ map = []
 x, y, start_x, start_y = 0, 0, 0, 0
 disallowed_percent = 0
 
-# Find disallowed percent for grid
-def find_disallowed():
-    full_area = len(map) * len(map[0])
-    disallowed_area = 0
-    for line in map:
-        for char in line:
-            if char == "#":
-                disallowed_area += 1
-    return 1 - (disallowed_area/full_area)
+# DP based approach
+def dpwalk(distance, sx, sy):
+    # Directions of movement - 4 degrees of freedom
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    # Initialize the DP table
+    dp = [[[0 for _ in range(distance + 1)] for _ in range(len(map[0]))] for _ in range(len(map))]
+
+    # Base case
+    dp[sx][sy][0] = 1
+
+    # Fill the DP table
+    for k in range(1, distance + 1):
+        for i in range(len(map)):
+            for j in range(len(map[0])):
+                for di, dj in directions:
+                    ni, nj = i + di, j + dj
+                    if 0 <= ni < len(map) and 0 <= nj < len(map[0]) and map[nj][ni]!="#":
+                        dp[ni][nj][k] += dp[i][j][k - 1]
+
+    return dp
+
+# Get plots reachable on a given dp table
+def get_plots(dp_table,dist):
+    plots = 0
+    for ymap in range(len(map)):
+        for xmap in range(len(map[0])):
+            if map[ymap][xmap] != "#" and dp_table[xmap][ymap][dist] > 0:
+                plots += 1
+    return plots
 
 # Read input file
 with open('inputfile.txt','r') as file:
@@ -30,16 +50,18 @@ for line in input_data.splitlines():
     y += 1
 
 # Sum
-# Find disallowed percent in the grid due to #
-disallowed_percent = find_disallowed()
+# Based on - https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+full_square_X = get_plots(dpwalk(129, 65, 65),129)
+full_square_O = get_plots(dpwalk(128, 65, 65),128)
+diamond_X = get_plots(dpwalk(65, 65, 65),65)
+diamond_O = get_plots(dpwalk(64, 65, 65),64)
 
-# Find theoretic area that could be covered
-max_area_one_axis = 2 * 26501365 + 1
-max_area = max_area_one_axis ** 2  # square
-max_area = math.ceil(max_area * 0.5)  # diamond because moving sideways takes away a step
-
-# Multiply by disallowed percent
-sum = max_area * disallowed_percent
+even = full_square_X
+odd = full_square_O
+n = 202300
+ecorn = even-diamond_O
+ocorn = odd-diamond_X
+sum = (((n+1)**2)*odd)+((n**2)*even)-((n+1)*ocorn)+(n*ecorn)
 
 # Answer
 print("PART 2")
