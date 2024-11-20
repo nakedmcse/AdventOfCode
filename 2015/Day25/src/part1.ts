3a19e07ce75ed26bf492028dc9fa5b955dad216c
Walker Aldridge <daldridge@lairdscomputer.com>
2024-11-20 11:57:43 -0600

    2015 Day 25

--- 2015/Day25/src/part1.ts
+++ 2015/Day25/src/part1.ts
@@ -10,42 +10,20 @@ function generateCode(codeNum: number): bigint {
     return retval;
 }
 
-function genLookupTable(t: number[][], s: number): void {
-    let curnum = 1;
-    let row = 0;
-    let col = 0;
-    for(let i = 0; i < (2 * s - 1); i++) {
-        if(i < s) {
-            row = 0;
-            col = i;
-        } else {
-            row = i - s + 1;
-            col = s - 1;
-        }
-
-        while(row < s && col >= 0) {
-            t[row][col] = curnum;
-            curnum++;
-            row++;
-            col--;
-        }
+function getCodeNumber(r: number, c: number): number {
+    let retval = 0;
+    for(let i = 0; i < r+c-1; i++) {
+        retval += i;
     }
+    return retval+c;
 }
 
 async function main() {
     const givenR: number = 2981;
     const givenC: number = 3075;
-    const codeTable: number[][] = Array.from({ length: 3075 }, () => Array(3075).fill(0));
-    genLookupTable(codeTable, 3075);
-    const codenumber: number = codeTable[givenC-1][givenR-1];
+    const codenumber: number = getCodeNumber(givenR, givenC);
     const code: bigint = generateCode(codenumber);
     console.log(`Part 1 Code: ${code}`);
-    console.log(generateCode(codeTable[5][0]));
-    console.log(generateCode(codeTable[5][1]));
-    console.log(generateCode(codeTable[5][2]));
-    console.log(generateCode(codeTable[5][3]));
-    console.log(generateCode(codeTable[5][4]));
-    console.log(generateCode(codeTable[5][5]));
 }
 
 main();
