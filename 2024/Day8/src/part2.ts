9c62e71332a0f6a1f2c721c3e01f6fb11a24d5e3
Walker Aldridge <daldridge@lairdscomputer.com>
2024-12-08 00:14:58 -0600

    2024 Day 8

---
+++ 2024/Day8/src/part2.ts
@@ -0,0 +1,61 @@
+//2024 day 8 part 2
+import {AocLib} from "./aocLib";
+
+function findAntiNodes(a: number[], b: number[]): number[][] {
+    const retval = [];
+    const yDiff = b[0]-a[0];
+    const xDiff = b[1]-a[1];
+    for(let i = 0; i<99; i++) {
+        retval.push([b[0]+(yDiff * i), b[1]+(xDiff * i)]);
+        retval.push([a[0]-(yDiff * i), a[1]-(xDiff * i)]);
+    }
+    return retval;
+}
+
+function generateAllPairs(arr: number[][]): number[][][] {
+    const pairs: number[][][] = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            pairs.push([[...arr[i]], [...arr[j]]]);
+        }
+    }
+    return pairs;
+}
+
+async function main() {
+    const freqLoc: Map<string, number[][]> = new Map<string, number[][]>();
+    const antiNodes: number[][] = [];
+    const lines = await AocLib.readFile('input.txt');
+    if (lines) {
+        for(let y = 0; y<lines.length; y++) {
+            for(let x = 0; x<lines[0].length; x++) {
+                const splitline = lines[y].split('');
+                if(splitline[x] === '.') continue;
+                const loc = [y, x];
+                if(freqLoc.has(splitline[x])) {
+                    let curloc = freqLoc.get(splitline[x]) ?? [];
+                    curloc.push([...loc]);
+                    freqLoc.set(splitline[x], curloc);
+                } else {
+                    freqLoc.set(splitline[x], [[...loc]]);
+                }
+            }
+        }
+
+        for(const [key, locs] of freqLoc) {
+            const pairs = generateAllPairs(locs);
+            for(const [p1,p2] of pairs) {
+                const anodes = findAntiNodes(p1, p2);
+                for(const anode of anodes) {
+                    if(antiNodes.filter(x => x[0] === anode[0] && x[1] === anode[1]).length === 0) antiNodes.push([...anode]);
+                }
+            }
+        }
+
+        const inFieldAntiNodes = antiNodes.filter(x => x[0]>=0 && x[0] < lines.length && x[1]>=0 && x[1]<lines[0].length);
+
+        console.log(`Part 2 Sum: ${inFieldAntiNodes.length}`);
+    }
+}
+
+main();
