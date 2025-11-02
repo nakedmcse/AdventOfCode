1ca105ec12618f03d8c4cf4f1ab69e254c548995
Walker Aldridge <daldridge@lairdscomputer.com>
2025-11-02 00:38:05 -0500

    2016 Day 2

---
+++ 2016/Day2/src/part1.ts
@@ -0,0 +1,45 @@
+//2016 Day 2 Part 1
+import {AocLib} from "./aocLib";
+
+type move = "U" | "D" | "L" | "R";
+
+const keypad: string[][] = [['1','2','3'],['4','5','6'],['7','8','9']];
+
+class point {
+    public constructor(public x: number = 1, public y: number = 1) {}
+
+    public move(direction: move): void {
+        switch (direction) {
+            case 'U':
+                this.y = this.y === 0 ? 0 : this.y - 1;
+                break;
+            case 'D':
+                this.y = this.y === 2 ? 2 : this.y + 1;
+                break;
+            case 'L':
+                this.x = this.x === 0 ? 0 : this.x - 1;
+                break;
+            case 'R':
+                this.x = this.x === 2 ? 2 : this.x + 1;
+                break;
+        }
+    }
+}
+
+async function main() {
+    const lines = await AocLib.readFile('input.txt');
+    if (lines) {
+        let code = "";
+        const location:point = new point();
+        for(const line of lines) {
+            for(const dir of line) {
+                location.move(dir as move);
+            }
+            code += keypad[location.y][location.x];
+        }
+
+        console.log(`Part 1 Code: ${code}`);
+    }
+}
+
+main();
---
+++ 2016/Day2/src/part2.ts
@@ -0,0 +1,52 @@
+//2016 Day 2 Part 2
+import {AocLib} from "./aocLib";
+
+type move = "U" | "D" | "L" | "R";
+
+const keypad: string[][] = [['*','*','1','*','*'],['*','2','3','4','*'],['5','6','7','8','9'],
+    ['*','A','B','C','*'],['*','*','D','*','*']];
+
+class point {
+    public constructor(public x: number = 0, public y: number = 2) {}
+
+    private checkValid(x: number, y: number): boolean {
+        if (x < 0 || x > 4 || y < 0 || y > 4) return false;
+        if (keypad[y][x] === '*') return false;
+        return true;
+    }
+
+    public move(direction: move): void {
+        switch (direction) {
+            case 'U':
+                this.y = this.checkValid(this.x, this.y-1) ? this.y - 1 : this.y;
+                break;
+            case 'D':
+                this.y = this.checkValid(this.x, this.y+1) ? this.y + 1 : this.y;
+                break;
+            case 'L':
+                this.x = this.checkValid(this.x-1, this.y) ? this.x - 1 : this.x;
+                break;
+            case 'R':
+                this.x = this.checkValid(this.x+1, this.y) ? this.x + 1 : this.x;
+                break;
+        }
+    }
+}
+
+async function main() {
+    const lines = await AocLib.readFile('input.txt');
+    if (lines) {
+        let code = "";
+        const location:point = new point();
+        for(const line of lines) {
+            for(const dir of line) {
+                location.move(dir as move);
+            }
+            code += keypad[location.y][location.x];
+        }
+
+        console.log(`Part 2 Code: ${code}`);
+    }
+}
+
+main();
