db435b1c6940b812f9756f4cfdc8af214c21c5c5
walker <daldridge@lairdscomputer.com>
2025-11-13 09:46:43 +0000

    2016 Day 12 Refactor

--- 2016/Day12/src/part1.ts
+++ 2016/Day12/src/part1.ts
@@ -2,97 +2,69 @@
 import {AocLib} from "./aocLib";
 
 class vm {
-    public constructor(public program: string[], public pc: number, public a: number, public b: number, public c:number, public d:number) {}
+    private readonly validRegisters: string[] = ['a','b','c','d'];
+    private Actions:Map<string,Function> = new Map<string,Function>();
+
+    private incAction = (arg1: string, arg2: string) : void  => {
+        if (this.validRegisters.includes(arg1)) {
+            this.registers[this.validRegisters.indexOf(arg1)]++;
+        }
+        this.pc++;
+    }
+
+    private decAction = (arg1: string, arg2: string) : void => {
+        if (this.validRegisters.includes(arg1)) {
+            this.registers[this.validRegisters.indexOf(arg1)]--;
+        }
+        this.pc++;
+    }
+
+    private cpyAction = (arg1: string, arg2: string) : void => {
+        if (this.validRegisters.includes(arg1) && this.validRegisters.includes(arg2)) {
+            this.registers[this.validRegisters.indexOf(arg2)] = this.registers[this.validRegisters.indexOf(arg1)];
+        }
+        else if (this.validRegisters.includes(arg2)) {
+            const num = parseInt(arg1, 10);
+            this.registers[this.validRegisters.indexOf(arg2)] = num;
+        }
+        this.pc++;
+    }
+
+    private jnzAction = (arg1: string, arg2: string) : void => {
+        const dist = parseInt(arg2, 10);
+        if ((arg1 === 'a' && this.registers[0] > 0) || (arg1 === 'b' && this.registers[1] > 0)
+            || (arg1 === 'c' && this.registers[2] > 0) || (arg1 === 'd' && this.registers[3] > 0)) {
+            this.pc += dist;
+        } else {
+            const num = parseInt(arg1, 10);
+            if (num > 0) {
+                this.pc += dist;
+            } else {
+                this.pc++;
+            }
+        }
+    }
+
+    private registerActions() {
+        this.Actions.set('inc', this.incAction);
+        this.Actions.set('dec', this.decAction);
+        this.Actions.set('jnz', this.jnzAction);
+        this.Actions.set('cpy', this.cpyAction);
+    }
+
+    public constructor(public program: string[], public pc: number, public registers: number[]) {
+        this.registerActions();
+    }
 
     public ExecuteNext() {
         if (this.pc >= this.program.length) return;  // past end of code
         const parsed = this.program[this.pc].split(' ');
-        switch(parsed[0]) {
-            case 'inc':
-                if (parsed[1] === 'a') {
-                    this.a++;
-                } else if (parsed[1] === 'b') {
-                    this.b++;
-                } else if (parsed[1] === 'c') {
-                    this.c++;
-                } else if (parsed[1] === 'd') {
-                    this.d++;
-                }
-                this.pc++;
-                break;
-            case 'dec':
-                if (parsed[1] === 'a') {
-                    this.a--;
-                } else if (parsed[1] === 'b') {
-                    this.b--;
-                } else if (parsed[1] === 'c') {
-                    this.c--;
-                } else if (parsed[1] === 'd') {
-                    this.d--;
-                }
-                this.pc++;
-                break;
-            case 'cpy':
-                if (parsed[1] === 'a') {
-                    if (parsed[2] === 'b') {
-                        this.b = this.a;
-                    } else if (parsed[2] === 'c') {
-                        this.c = this.a;
-                    } else if (parsed[2] === 'd') {
-                        this.d = this.a;
-                    }
-                } else if (parsed[1] === 'b') {
-                    if (parsed[2] === 'a') {
-                        this.a = this.b;
-                    } else if (parsed[2] === 'c') {
-                        this.c = this.b;
-                    } else if (parsed[2] === 'd') {
-                        this.d = this.b;
-                    }
-                } else if (parsed[1] === 'c') {
-                    if (parsed[2] === 'a') {
-                        this.a = this.c;
-                    } else if (parsed[2] === 'b') {
-                        this.b = this.c;
-                    } else if (parsed[2] === 'd') {
-                        this.d = this.c;
-                    }
-                } else if (parsed[1] === 'd') {
-                    if (parsed[2] === 'b') {
-                        this.b = this.d;
-                    } else if (parsed[2] === 'c') {
-                        this.c = this.d;
-                    } else if (parsed[2] === 'a') {
-                        this.a = this.d;
-                    }
-                } else {
-                    const num = parseInt(parsed[1], 10);
-                    if (parsed[2] === 'a') {
-                        this.a = num;
-                    } else if (parsed[2] === 'b') {
-                        this.b = num;
-                    } else if (parsed[2] === 'c') {
-                        this.c = num;
-                    } else if (parsed[2] === 'd') {
-                        this.d = num;
-                    }
-                }
-                this.pc++;
-                break;
-            case 'jnz':
-                const dist = parseInt(parsed[2], 10);
-                if ((parsed[1] === 'a' && this.a > 0) || (parsed[1] === 'b' && this.b > 0)
-                    || (parsed[1] === 'c' && this.c > 0) || (parsed[1] === 'd' && this.d > 0)) {
-                    this.pc += dist;
-                } else {
-                    const num = parseInt(parsed[1], 10);
-                    if (num > 0) {
-                        this.pc += dist;
-                    } else {
-                        this.pc++;
-                    }
-                }
-                break;
+        const action = this.Actions.get(parsed[0]);
+        if (action) {
+            action(parsed[1], parsed[2]);  // execute defined action
+        }
+        else {
+            this.pc++;  // skip over undefined instruction
         }
     }
 }
@@ -100,11 +72,11 @@ class vm {
 async function main() {
     const lines = await AocLib.readFile('input.txt');
     if (lines) {
-        const virt = new vm (lines, 0, 0, 0, 0, 0);
+        const virt = new vm (lines, 0, [0, 0, 0, 0]);
         while(virt.pc < lines.length) {
             virt.ExecuteNext();
         }
-        console.log(`Part 1 A reg value: ${virt.a}`);
+        console.log(`Part 1 A reg value: ${virt.registers[0]}`);
     }
 }
 
