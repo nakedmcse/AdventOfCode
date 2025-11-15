7a4012d3f5db6e85ccb47f96c166b69765c98f7c
walker <daldridge@lairdscomputer.com>
2025-11-15 10:25:17 +0000

    2016 Day 16

---
+++ 2016/Day16/src/part2.ts
@@ -0,0 +1,36 @@
+//2016 Day 16 Part 2
+function generateDragon(a: string): string {
+    let b = '';
+    for (let i = a.length-1; i >= 0; i--) {
+        b += a[i] === '1' ? '0' : '1';
+    }
+    return a+'0'+b;
+}
+
+function checksum(s: string): string {
+    let chksum = '';
+    for (let i = 0; i < s.length; i += 2) {
+        if (s[i] === s[i+1]) {
+            chksum += '1';
+        }
+        else {
+            chksum += '0';
+        }
+    }
+    if (chksum.length%2 === 0) {
+        chksum = checksum(chksum);
+    }
+    return chksum;
+}
+
+async function main() {
+    let data: string = "01111010110010011";
+    const targetLength: number = 35651584;
+    while (data.length < targetLength) {
+        data = generateDragon(data);
+    }
+    const csum: string = checksum(data.substring(0, targetLength));
+    console.log(`Part 2 Checksum: ${csum}`);
+}
+
+main();
