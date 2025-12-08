f5b4a3dcd566ea2705bbe755ba388ea43419f7c7
Walker Aldridge <daldridge@lairdscomputer.com>
2025-12-08 15:00:24 -0600

    2025 Day 8 P2 better end detection

--- 2025/Day8/src/part2.ts
+++ 2025/Day8/src/part2.ts
@@ -1,7 +1,7 @@
 //2025 Day 8 Part 2
 import {AocLib} from "./aocLib";
 
-type position = {i: number, x: number, y: number, z: number, connected: number[]};
+type position = {i: number, x: number, y: number, z: number};
 type pair = {i1: number, i2: number, dist:number};
 
 const junctions: position[] = [];
@@ -29,25 +29,22 @@ async function main() {
         for(const line of lines) {
             const numbers = AocLib.getNumbers(line);
             if (numbers) {
-                junctions.push({i: i++, x: numbers[0], y: numbers[1], z: numbers[2], connected:[]});
+                junctions.push({i: i++, x: numbers[0], y: numbers[1], z: numbers[2]});
             }
         }
 
         console.time('main');
         getSortedPairs();
+        const targetLength = junctions.length;
+        const connected = new Set<number>();
         let searching = true;
         while(searching) {
             const c = pairs.pop() ?? {i1: -1, i2: -1, dist:-1};
-            junctions[c.i1].connected.push(c.i2);
-            junctions[c.i2].connected.push(c.i1);
+            connected.add(c.i1);
+            connected.add(c.i2);
 
             searching = false;
-            for (let i = 0; i < junctions.length; i++) {
-                if (junctions[i].connected.length === 0) {
-                    searching = true;
-                    break;
-                }
-            }
+            searching = connected.size !== targetLength;
 
             if (!searching) {
                 sum = junctions[c.i1].x * junctions[c.i2].x;
